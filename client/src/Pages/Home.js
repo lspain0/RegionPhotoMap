d2255bd56caa45debecc8e390cfa5f7383f1cbca
lspain0 <110966000+lspain0@users.noreply.github.com>
2025-07-10 13:31:12 +1200

    Added placeholders for essential pages/components

---
+++ client/src/Pages/Home.js
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+    <div className="home-container">
+        <header className="home-header">
+        <p>
+            test
+        </p>
+        </header>
+    </div>
+    )
+}
+
+export default Home;
